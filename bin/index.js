46e8d5486e93d33ee0fc62cc7302aeebc0530858
Magic Len <len@magiclen.org>
2025-10-16 00:46:59 +0800

    support passing signals

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { spawnSync } from "node:child_process";
+import { exec, spawn } from "node:child_process";
 const argv = process.argv;
 if (argv.length <= 3) {
     process.exit(0);
@@ -7,7 +7,7 @@ if (argv.length <= 3) {
 const nodeEnv = argv[2];
 const command = argv[3];
 const args = argv.slice(4);
-spawnSync(command, args, {
+const child = spawn(command, args, {
     env: {
         ...process.env,
         NODE_ENV: nodeEnv,
@@ -15,4 +15,36 @@ spawnSync(command, args, {
     argv0: argv[0],
     shell: true,
     stdio: "inherit",
+    detached: true,
+});
+if (typeof child.pid === "undefined") {
+    process.exit(1);
+}
+const signals = [
+    "SIGINT",
+    "SIGTERM",
+    "SIGHUP",
+];
+if (process.platform === "win32") {
+    signals.push("SIGBREAK", "SIGQUIT");
+    for (const signal of signals) {
+        process.on(signal, () => {
+            exec(`taskkill /PID ${child.pid} /T`);
+        });
+    }
+}
+else {
+    signals.push("SIGUSR1", "SIGUSR2");
+    for (const signal of signals) {
+        process.on(signal, () => {
+            try {
+                process.kill(-child.pid, signal);
+            }
+            catch {
+            }
+        });
+    }
+}
+child.on("exit", (code, signal) => {
+    process.exit(code ?? (signal ? 1 : 0));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { spawnSync } from "node:child_process";
+import { exec, spawn } from "node:child_process";
 
 const argv = process.argv;
 
@@ -12,7 +12,7 @@ const nodeEnv = argv[2];
 const command = argv[3];
 const args = argv.slice(4);
 
-spawnSync(command, args, {
+const child = spawn(command, args, {
     env: {
         ...process.env,
         NODE_ENV: nodeEnv,
@@ -20,4 +20,43 @@ spawnSync(command, args, {
     argv0: argv[0],
     shell: true,
     stdio: "inherit",
+    detached: true,
+});
+
+if (typeof child.pid === "undefined") {
+    process.exit(1);
+}
+
+const signals: NodeJS.Signals[] = [
+    "SIGINT",
+    "SIGTERM",
+    "SIGHUP",
+];
+
+if (process.platform === "win32") {
+    signals.push("SIGBREAK", "SIGQUIT");
+
+    for (const signal of signals) {
+        process.on(signal, () => {
+            exec(`taskkill /PID ${child.pid} /T`);
+        });
+    }
+} else {
+    signals.push("SIGUSR1", "SIGUSR2");
+
+    for (const signal of signals) {
+        process.on(signal, () => {
+            try {
+                // the pid has been checked before
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                process.kill(-child.pid!, signal);
+            } catch {
+                // do nothing
+            }
+        });
+    }
+}
+
+child.on("exit", (code, signal) => {
+    process.exit(code ?? (signal ? 1 : 0));
 });
